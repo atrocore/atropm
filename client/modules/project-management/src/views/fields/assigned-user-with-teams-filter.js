a22fad3e8bdbaffe5fa8aa090386e44ca85e5e64
r.ratsun <r.ratsun@gmail.com>
2021-05-06 11:58:43 +0300

    .

--- client/modules/project-management/src/views/fields/assigned-user-with-teams-filter.js
+++ client/modules/project-management/src/views/fields/assigned-user-with-teams-filter.js
@@ -1,21 +1,30 @@
 /*
- * This file is part of premium software, which is NOT free.
- * Copyright (c) AtroCore UG (haftungsbeschränkt).
- *
- * This Software is the property of AtroCore UG (haftungsbeschränkt) and is
- * protected by copyright law - it is NOT Freeware and can be used only in one
- * project under a proprietary license, which is delivered along with this program.
- * If not, see <https://atropim.com/eula> or <https://atrodam.com/eula>.
- *
- * This Software is distributed as is, with LIMITED WARRANTY AND LIABILITY.
- * Any unauthorised use of this Software without a valid license is
- * a violation of the License Agreement.
- *
- * According to the terms of the license you shall not resell, sublicense,
- * rent, lease, distribute or otherwise transfer rights or usage of this
- * Software or its derivatives. You may modify the code of this Software
- * for your own needs, if source code is provided.
- */
+* This file is part of AtroPM.
+*
+* AtroPM - Open Source Project Management application.
+* Copyright (C) 2021 AtroCore UG (haftungsbeschränkt).
+* Website: https://atrocore.com
+*
+* AtroPM is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* AtroPM is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with AtroPIM. If not, see http://www.gnu.org/licenses/.
+*
+* The interactive user interfaces in modified source and object code versions
+* of this program must display Appropriate Legal Notices, as required under
+* Section 5 of the GNU General Public License version 3.
+*
+* In accordance with Section 7(b) of the GNU General Public License version 3,
+* these Appropriate Legal Notices must retain the display of the "AtroPM" word.
+*/
 
 Espo.define('project-management:views/fields/assigned-user-with-teams-filter', ['views/fields/assigned-user', 'project-management:views/fields/link'], function (Dep, Link) {
 
