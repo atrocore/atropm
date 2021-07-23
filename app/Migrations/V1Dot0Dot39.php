40220bb6b5cc1133027704c85690d12ebfe85054
r.ratsun <r.ratsun@gmail.com>
2021-07-23 13:00:32 +0300

    .

---
+++ app/Migrations/V1Dot0Dot39.php
@@ -0,0 +1,54 @@
+<?php
+/*
+* This file is part of AtroPM.
+*
+* AtroPM - Open Source Project Management application.
+* Copyright (C) 2021 AtroCore UG (haftungsbeschränkt).
+* Website: https://atrocore.com
+*
+* AtroPM is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* AtroPM is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with AtroPIM. If not, see http://www.gnu.org/licenses/.
+*
+* The interactive user interfaces in modified source and object code versions
+* of this program must display Appropriate Legal Notices, as required under
+* Section 5 of the GNU General Public License version 3.
+*
+* In accordance with Section 7(b) of the GNU General Public License version 3,
+* these Appropriate Legal Notices must retain the display of the "AtroPM" word.
+*/
+
+declare(strict_types=1);
+
+namespace ProjectManagement\Migrations;
+
+/**
+ * Migration for version 1.0.39
+ */
+class V1Dot0Dot39 extends V1Dot0Dot1
+{
+    /**
+     * @inheritDoc
+     */
+    public function up(): void
+    {
+        $this->execute("ALTER TABLE `issue` ADD file_id VARCHAR(24) DEFAULT NULL COLLATE utf8mb4_unicode_ci");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function down(): void
+    {
+        $this->execute("ALTER TABLE `issue` DROP file_id");
+    }
+}
